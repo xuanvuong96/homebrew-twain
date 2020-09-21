5648c76f18300e9acdf7f88cb27950566c9eb9c0
ramsey <ramsey@ramseys-iMac.local>
2020-09-21 00:44:55 -0700

    a

--- twain.rb
+++ twain.rb
@@ -1,11 +1,14 @@
+require "language/node"
+
 class Twain < Formula
   desc "Cross-platform Text Expander written in Rust"
   homepage "https://github.com/MrLolo102/twain"
-  url "https://github.com/MrLolo102/twain/releases/download/1.0/twaind"
-  sha256 "2329cf961f1bfcfa35aadfd395141cbf7db49faf043884f8b387f0129c795657"
+  url "https://github.com/MrLolo102/twain/archive/1.0.tar.gz"
+  sha256 "2ed7f9a1cc874118e265dd5c064baba31c4db93fa96a8dea0b4b0b99224c59d0"
   version "0.1.0"
 
   def install
-    bin.install "80229f33a32ac1ab8c6e4a4b2b4644961c7474ba93e71b5967bd07fbd60d9e74--twaind"
+    bin.install "twain"
   end
 end
+
